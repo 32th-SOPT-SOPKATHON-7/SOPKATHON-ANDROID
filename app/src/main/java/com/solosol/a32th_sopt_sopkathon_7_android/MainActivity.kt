ea49b1ebb9a4c8be3dd09774fb856832f8516252
KwakEuiJin <yuijin0909@gmail.com>
2023-05-18 23:23:35 +0900

    First Commit

---
+++ app/src/main/java/com/solosol/a32th_sopt_sopkathon_7_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.solosol.a32th_sopt_sopkathon_7_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
