77ddcd3f04930716091e02bfb0a9787d633c8d03
jooyyoo <aliceyun9@naver.com>
2023-05-21 02:23:31 +0900

    [feat] ui 레이아웃 구현, 좋아요 미완성

--- app/src/main/java/com/solosol/a32th_sopt_sopkathon_7_android/MainActivity.kt
+++ app/src/main/java/com/solosol/a32th_sopt_sopkathon_7_android/MainActivity.kt
@@ -3,19 +3,26 @@ package com.solosol.a32th_sopt_sopkathon_7_android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import com.solosol.a32th_sopt_sopkathon_7_android.base.BaseViewBindingActivity
-import com.solosol.a32th_sopt_sopkathon_7_android.databinding.ActivityMainBinding
+import com.solosol.a32th_sopt_sopkathon_7_android.databinding.ActivityCommentBinding
 import com.solosol.a32th_sopt_sopkathon_7_android.extension.showToast
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 
-class MainActivity : BaseViewBindingActivity<ActivityMainBinding>() {
+
+class MainActivity : BaseViewBindingActivity<ActivityCommentBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         /** setContentView 하지 말기 **/
         showToast("화이팅")
+
+        /*val    now: Instant = Clock.System.now()
+        Log.d("time", now.toString())*/
     }
 
-    override fun setBinding(layoutInflater: LayoutInflater): ActivityMainBinding {
-        return ActivityMainBinding.inflate(layoutInflater)
+    override fun setBinding(layoutInflater: LayoutInflater): ActivityCommentBinding {
+        return ActivityCommentBinding.inflate(layoutInflater)
     }
 }
