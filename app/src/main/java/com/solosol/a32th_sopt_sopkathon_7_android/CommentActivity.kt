939dc098f3cf1a652463476d2669272b58a0aa93
jooyyoo <aliceyun9@naver.com>
2023-05-21 05:55:29 +0900

    [feat] jooyoung

--- app/src/main/java/com/solosol/a32th_sopt_sopkathon_7_android/CommentActivity.kt
+++ app/src/main/java/com/solosol/a32th_sopt_sopkathon_7_android/CommentActivity.kt
@@ -2,41 +2,59 @@ package com.solosol.a32th_sopt_sopkathon_7_android
 
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.inputmethod.InputMethodManager
-import android.widget.ImageButton
-import androidx.core.content.getSystemService
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.solosol.a32th_sopt_sopkathon_7_android.api.ApiFactory.soptService
-import com.solosol.a32th_sopt_sopkathon_7_android.api.SoptService
+import com.solosol.a32th_sopt_sopkathon_7_android.api.model.request.CreateCommentRequest
+import com.solosol.a32th_sopt_sopkathon_7_android.api.model.response.DetailArticleResponse
 import com.solosol.a32th_sopt_sopkathon_7_android.base.BaseViewBindingActivity
 import com.solosol.a32th_sopt_sopkathon_7_android.databinding.ActivityCommentBinding
 import com.solosol.a32th_sopt_sopkathon_7_android.extension.showToast
+import com.solosol.a32th_sopt_sopkathon_7_android.sample.CommentAdapter
 import kotlinx.coroutines.launch
-import kotlinx.datetime.Clock
-import kotlinx.datetime.Instant
 
 
-class CActivity : BaseViewBindingActivity<ActivityCommentBinding>() {
+class CommentActivity : BaseViewBindingActivity<ActivityCommentBinding>() {
+    val postId: Int = 1
+    val commentAdapter = CommentAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         hidekeyboard()
 
-        binding.btReport.setOnClickListener(
-            showToast("게시글이 신고되었습니다.")
-        )
+        binding.btReport.setOnClickListener {
+            showToast("신고완료")
+        }
 
         lifecycleScope.launch {
             val response = soptService.getDetailArticle(postId)
             if (response.isSuccessful) {
-                binding.tvTitlePost.text = response.body().data?.
+                binding.tvTitlePost.text = response.body()?.data?.title
+                binding.tvPostTime.text = response.body()?.data?.createdAt?.substring(0 until 10)
+                binding.tvArticle.text = response.body()?.data?.content
+                val commentList: List<DetailArticleResponse.Data.Comment?>? =
+                    response.body()?.data?.comments
+                commentAdapter.submitList(commentList)
             } else {
+                showToast("서버통신 실패")
+            }
+        }
 
+        lifecycleScope.launch {
+            val response = soptService.getDetailArticle(postId)
+            val comment_response = soptService.postCreateComment(postId, CreateCommentRequest(binding.etWriteComment.text.toString()))
+            if (comment_response.isSuccessful){
+                soptService.getDetailArticle(postId)
+                val commentList: List<DetailArticleResponse.Data.Comment?>? =
+                    response.body()?.data?.comments
+                commentAdapter.submitList(commentList)
+            }
+            else{
+                showToast("서버통신 실패")
             }
         }
 
@@ -54,6 +72,16 @@ class CActivity : BaseViewBindingActivity<ActivityCommentBinding>() {
                 this.currentFocus?.windowToken,
                 InputMethodManager.HIDE_NOT_ALWAYS
             )
+        }
+    }
+
+    private fun closeBtnClickListener() {
+        binding.btBack.setOnClickListener {
+            this.finish()
+//            val intent = Intent(this, SubwaySearchActivity::class.java).apply {
+//                putExtra("stationName", binding.tvPostStationName.text.toString())
+//            }
+//            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/solosol/a32th_sopt_sopkathon_7_android/sample/CommentAdapter.kt
+++ app/src/main/java/com/solosol/a32th_sopt_sopkathon_7_android/sample/CommentAdapter.kt
@@ -4,13 +4,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.solosol.a32th_sopt_sopkathon_7_android.api.model.response.DetailArticleResponse
 import com.solosol.a32th_sopt_sopkathon_7_android.databinding.ItemCommentBinding
 import com.solosol.a32th_sopt_sopkathon_7_android.util.DiffCallback
 
 
-class CommentAdapter : ListAdapter<CommentData, CommentAdapter.CommentViewHolder>(
-    CommentDiffCallback
-) {
+class CommentAdapter : ListAdapter<DetailArticleResponse.Data.Comment, CommentAdapter.CommentViewHolder>(CommentDiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
         return CommentViewHolder(ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false))
@@ -21,13 +20,19 @@ class CommentAdapter : ListAdapter<CommentData, CommentAdapter.CommentViewHolder
     }
 
     class CommentViewHolder(private val binding: ItemCommentBinding) : ViewHolder(binding.root) {
-        fun bind(item: CommentData) {
+        fun bind(item: DetailArticleResponse.Data.Comment) {
 
         }
     }
 
+    fun addListItem(newItem: DetailArticleResponse.Data.Comment){
+        val newItemList = mutableListOf<DetailArticleResponse.Data.Comment>()
+        newItemList.add(newItem)
+        submitList(newItemList)
+    }
+
     companion object {
         private val CommentDiffCallback =
-            DiffCallback<CommentData>(id = { old, new -> old.Comment == new.Comment})
+            DiffCallback<DetailArticleResponse.Data.Comment>(id = { old, new -> old.commentId == new.commentId})
     }
 }
